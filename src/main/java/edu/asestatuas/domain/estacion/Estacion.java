cc0e1f7bd0d14a9692fd5cda85ad92af308e20e8
ccastroto <carloscastroaldrover@gmail.com>
2025-02-20 13:49:36 +0100

    feat(estacion): Añadir la parte de Anclajes para el caso test
    Signed-off-by: ccastroto <carloscastroaldrover@gmail.com>

--- src/main/java/edu/asestatuas/domain/estacion/Estacion.java
+++ src/main/java/edu/asestatuas/domain/estacion/Estacion.java
@@ -6,10 +6,12 @@ public class Estacion {
 
     private final int id;
     private final String direccion;
+    private final Anclajes anclajes;
 
-    public Estacion(int id, String direccion, int ) {
+    public Estacion(int id, String direccion, int numAnclajes) {
         this.id = id;
         this.direccion = direccion;
+        this.anclajes =  new Anclajes(numAnclajes);
     }
 
     private int getId() {
@@ -20,6 +22,16 @@ public class Estacion {
     }
     @Override
     public String toString(){
-        return getDireccion();
+        return  "id:" + getId() + '\n' +
+                "direccion:" + getDireccion() + '\n';
+    }
+    private Anclaje[] anclajes(){
+        return this.anclajes.anclajes();
+    }
+    private int numAnclajes(){
+        return this.anclajes.numAnclajes();
+    }
+    public void consultarEstacion() {
+        System.out.println(this);
     }
 }
