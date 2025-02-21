f24c2fa22b3b657368e2fabd1e67ce9c70ae4086
ccastroto <carloscastroaldrover@gmail.com>
2025-02-21 13:02:44 +0100

    test(BicicletaTest): Añadir el fichero de casos tests para la bicicleta
    Signed-off-by: ccastroto <carloscastroaldrover@gmail.com>

---
+++ src/test/java/domain/bicicleta/BicicletaTest.java
@@ -0,0 +1,25 @@
+package domain.bicicleta;
+
+import edu.asestatuas.domain.bicicleta.Bicicleta;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BicicletaTest {
+
+    private Bicicleta bicicleta = null;
+    @Before
+    public void setup_bicicleta() {
+        bicicleta = new Bicicleta(101);
+    }
+
+    @Test
+    public void test_constructor_bicicleta() {
+        // ARRANGE
+        int idExpected = 101;
+        // ACT
+        int id = bicicleta.getId();
+        // ASSERT
+        assertEquals(idExpected, id);
+    }
+}
