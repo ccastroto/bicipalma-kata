f12baaef95cc6f8bdc4d5446e5e10de0e09baa57
ccastroto <carloscastroaldrover@gmail.com>
2025-02-21 11:16:20 +0100

    fix(bicipalma): Arreglar error para funciona el caso test
    Signed-off-by: ccastroto <carloscastroaldrover@gmail.com>

--- src/main/java/edu/asestatuas/domain/bicicleta/Bicicleta.java
+++ src/main/java/edu/asestatuas/domain/bicicleta/Bicicleta.java
@@ -1,4 +1,17 @@
 package edu.asestatuas.domain.bicicleta;
 
 public class Bicicleta {
+
+    private final int id;
+
+    public Bicicleta(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
+    @Override
+    public String toString() {
+        return Integer.toString(getId());
+    }
 }
