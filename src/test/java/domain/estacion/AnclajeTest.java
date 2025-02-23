d7b12bbe87c0e5e74f8f129545d5fffc80bc47f3
ccastroto <carloscastroaldrover@gmail.com>
2025-02-23 18:54:43 +0100

    feat(anclaje): Añadir caso test de si se puede anclar una bicicleta

--- src/test/java/domain/estacion/AnclajeTest.java
+++ src/test/java/domain/estacion/AnclajeTest.java
@@ -1,5 +1,6 @@
 package domain.estacion;
 
+import edu.asestatuas.domain.bicicleta.Bicicleta;
 import edu.asestatuas.domain.estacion.Anclaje;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,8 +10,12 @@ import static org.junit.Assert.*;
 public class AnclajeTest {
 
     private Anclaje anclaje;
+    private Bicicleta bici;
     @Before
-    public void setup_anclaje() {anclaje = new Anclaje();}
+    public void setup_anclaje() {
+        anclaje = new Anclaje();
+        bici = new Bicicleta(101); //Simular biciº
+    }
 
     @Test
     public void test_si_esta_ocupado(){
@@ -21,4 +26,14 @@ public class AnclajeTest {
     public void test_coge_bicicleta(){
         assertNotEquals(anclaje.getBici(), "");
     }
+   @Test
+   public void test_anclar_bicicleta(){
+        // Si está libre
+       assertFalse(anclaje.isOcupado());
+       // Se ancla
+       anclaje.anclarBici(bici);
+       // Check si está ocupado y bici asignada
+       assertTrue(anclaje.isOcupado());
+       assertEquals(bici, anclaje.getBici());
+   }
 }
