b1f3e195e0bd13fd5941eec3773d2e876cac3d15
ccastroto <castroaldrovercarlossangrinha@gmail.com>
2025-02-23 13:51:16 +0100

    feat(anclaje): Añadir las definciones de liberar la bicicleta y el toString

--- src/main/java/edu/asestatuas/domain/estacion/Anclaje.java
+++ src/main/java/edu/asestatuas/domain/estacion/Anclaje.java
@@ -5,11 +5,25 @@ import edu.asestatuas.domain.bicicleta.Bicicleta;
 public class Anclaje {
 
     private boolean ocupado = false;
-    private Bicicleta bicicleta;
+    private Bicicleta bici;
 
     public Anclaje() {};
 
     public boolean isOcupado(){
         return this.ocupado;
+    }
+
+    public Bicicleta getBici() {
+        return this.bici;
+    }
+    public void anclarBici(Bicicleta bici){
+        this.bici = bici;
+        this.ocupado = true;
+    }
+    public void liberarBici(){
+        this.bici = null;
+    }
+    public String toString(){
+        return "ocupado: " + Boolean.toString(isOcupado());
     }
 }
