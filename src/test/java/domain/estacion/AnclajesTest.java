992350fd4322b1a5e320134dadd9d951ed6c53f2
ccastroto <carloscastroaldrover@gmail.com>
2025-02-24 19:04:37 +0100

    test(anclajes): Añadir caso test de NumAnclaje y refactorizar testAnclaje

--- src/test/java/domain/estacion/AnclajesTest.java
+++ src/test/java/domain/estacion/AnclajesTest.java
@@ -17,11 +17,19 @@ public class AnclajesTest {
     }
     @Test
     public void testAnclajes(){
+        // Check si objeto no es null
         assertNotNull(anclajes);
+        // Check si array no es null
         assertNotNull(anclajes.anclajes());
+        // Chack tamaño array esperado
         assertEquals(NUM_ANCLAJES, anclajes.anclajes().length, + NUM_ANCLAJES);
+        // Check posición objecto
         for (Anclaje anclaje : anclajes.anclajes()){
             assertNotNull(anclaje);
         }
+    }
+    @Test
+    public void testNumAnclajes(){
+        assertEquals(NUM_ANCLAJES, anclajes.numAnclajes());
     }
 }
