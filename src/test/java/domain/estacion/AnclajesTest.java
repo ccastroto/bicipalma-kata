341c0840780cafad664afb2a98abcfbf70174eaf
ccastroto <carloscastroaldrover@gmail.com>
2025-02-25 13:47:17 +0100

    test(anclajes): Añadir caso test de OcuparAnclaje

--- src/test/java/domain/estacion/AnclajesTest.java
+++ src/test/java/domain/estacion/AnclajesTest.java
@@ -1,5 +1,6 @@
 package domain.estacion;
 
+import edu.asestatuas.domain.bicicleta.Bicicleta;
 import edu.asestatuas.domain.estacion.Anclaje;
 import edu.asestatuas.domain.estacion.Anclajes;
 import org.junit.Before;
@@ -10,10 +11,12 @@ public class AnclajesTest {
 
     private Anclajes anclajes;
     private static final int NUM_ANCLAJES = 6;
+    private Bicicleta bici;
 
     @Before
     public void setup_anclajes(){
         anclajes = new Anclajes(NUM_ANCLAJES);
+        bici = new Bicicleta(101);
     }
     @Test
     public void testAnclajes(){
@@ -21,7 +24,7 @@ public class AnclajesTest {
         assertNotNull(anclajes);
         // Check si array no es null
         assertNotNull(anclajes.anclajes());
-        // Chack tamaño array esperado
+        // Check tamaño array esperado
         assertEquals(NUM_ANCLAJES, anclajes.anclajes().length, + NUM_ANCLAJES);
         // Check posición objecto
         for (Anclaje anclaje : anclajes.anclajes()){
@@ -31,5 +34,15 @@ public class AnclajesTest {
     @Test
     public void testNumAnclajes(){
         assertEquals(NUM_ANCLAJES, anclajes.numAnclajes());
+    }
+    @Test
+    public void testOcuparAnclaje(){
+        int posicion = 3;
+        // Ocuaparlo
+        anclajes.ocuparAnclaje(posicion, bici);
+        // Check su posición está ocupado
+        assertTrue(anclajes.anclajes()[posicion].isOcupado());
+        // Check anclaje posición tiene la bici
+        assertEquals(bici, anclajes.anclajes()[posicion].getBici());
     }
 }
